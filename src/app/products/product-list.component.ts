8629f2092f815d9bd7ad0190391184deab5ed147
Curtis Crutchfield <JJ10333@tn.gov>
2020-03-02 09:45:24 -0600

    can filter list from input

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle = 'Product List';
     imageWidth = 50;
     imageMargin = 2;
     showImage = false;
-    listFilter = 'cart';
-    products: any[] = [
+
+    _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[] = [];
+  products: IProduct[]  = [
     {
         productId: 2,
         productName: 'Garden Cart',
-        ProductCode: 'GDN-0023',
+        productCode: 'GDN-0023',
         releaseDate: 'March 18, 2020',
         description: '15 gallon capacity rolling garden cart',
         price: 32.99,
@@ -24,16 +36,31 @@ export class ProductListComponent {
     {
         productId: 5,
         productName: 'Hammer',
-        ProductCode: 'TBX-0048',
+        productCode: 'TBX-0048',
         releaseDate: 'May 18, 2020',
-        description: 'Curved claw stell hammer',
+        description: 'Curved claw steel hammer',
         price: 2.9,
         starRating: 4.8,
         imageUrl: 'assets/images/hammer.png'
     }
 
     ];
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this._listFilter = 'cart';
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+          product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+      }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+    ngOnInit(): void {
+        console.log('In OnInit');
     }
 }
