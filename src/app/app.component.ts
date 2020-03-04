cc94fd10bef9d6f2c11cf378d1905d3790ea6c0b
Curtis Crutchfield <JJ10333@tn.gov>
2020-03-04 09:39:42 -0600

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
     <li><a class='nav-link' [routerLink]="['/products']">Product</a></li>
     </ul>
     </nav>
-    <div class='container'> 
+    <div class='container'>
       <router-outlet></router-outlet>
     </div>
   `
